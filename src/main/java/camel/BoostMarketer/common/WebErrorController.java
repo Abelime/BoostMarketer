d99b8d8b5b23752cb03a0ad5c968b3912905f5f2
daramG <sang5879@naver.com>
2024-04-01 21:21:38 +0900

    에러페이지

---
+++ src/main/java/camel/BoostMarketer/common/WebErrorController.java
@@ -0,0 +1,31 @@
+package camel.BoostMarketer.common;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if(status != null){
+            int statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "common/404error";
+            } else {
+                return "common/500error";
+            }
+        }
+
+        return "common/404error";
+    }
+
+}
+
