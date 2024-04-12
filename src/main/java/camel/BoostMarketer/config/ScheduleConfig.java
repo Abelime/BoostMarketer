c7d73988fa1ab884bf95df64ebacd652bb10d3a7
caeml <w8682694@naver.com>
2024-04-12 20:05:40 +0900

    키워드 고정 버그 수정

--- src/main/java/camel/BoostMarketer/config/ScheduleConfig.java
+++ src/main/java/camel/BoostMarketer/config/ScheduleConfig.java
@@ -2,12 +2,8 @@ package camel.BoostMarketer.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Scheduled;
 
 
 @Slf4j
@@ -17,20 +13,20 @@ public class ScheduleConfig {
 
     private final JobLauncher jobLauncher;
 
-    private final Job job;
-
-    @Scheduled(cron = "0 0 0 * * ?")
-    public void runBlogDataJob() {
-        try {
-            // JobParameters can be used to pass in dynamic values or simply a unique parameter to ensure job instance is unique
-            JobParameters params = new JobParametersBuilder()
-                    .addLong("time", System.currentTimeMillis())
-                    .toJobParameters();
-
-            jobLauncher.run(job, params);
-        } catch (Exception e) {
-            log.error("Failed to execute apiJob", e);
-        }
-    }
+//    private final Job job;
+//
+//    @Scheduled(cron = "0 0 0 * * ?")
+//    public void runBlogDataJob() {
+//        try {
+//            // JobParameters can be used to pass in dynamic values or simply a unique parameter to ensure job instance is unique
+//            JobParameters params = new JobParametersBuilder()
+//                    .addLong("time", System.currentTimeMillis())
+//                    .toJobParameters();
+//
+//            jobLauncher.run(job, params);
+//        } catch (Exception e) {
+//            log.error("Failed to execute apiJob", e);
+//        }
+//    }
 
 }
